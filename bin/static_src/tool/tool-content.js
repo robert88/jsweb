9428ecce4f3ea6d9c0159a4e4bbc0bd0d8c9cf1a
robert yin <robert yin>
2017-09-19 07:41:44 +0800

    r

--- bin/static_src/tool/tool-content.js
+++ bin/static_src/tool/tool-content.js
@@ -30,7 +30,40 @@
 		var w = $zoomWrap.width();
 		var h = $zoomWrap.height();
 		if( perWrapw != w || h != perWraph){
-			
+			var widthzoom = getResizeSize(w,1211,1800,1024);
+			var heightzoom = getResizeSize(h,619,900,500);
+			$zoomTarget.data("fullzoom",Math.min(widthzoom,heightzoom));
+			zoomPage();
 		}
+	}
+	function zoomPage(value){
+		var fullzoomvalue = $zoomTarget.data("fullzoom");
+		var reportmodevalue=  $zoomTarget.data("reportzoom");
+		var setZoom = $zoomTarget.data("setzoom")
+		value = value || reportmodevalue || setZoom || fullzoomvalue;
+
+		if(!value){
+			console.error("zoom value ==0");
+			return;
+		}
+		var rate = value/100;
+		var zoomOriginSize = {w:1211,h:619};//原始大小
+		var zoomSize = {w:rate*zoomOriginSize.w,h:rate*zoomOriginSize.h}//缩放之后的大小
+		var wrapSize = {w:$zoomWrap.width(),h:$zoomWrap.height()};//外框大小
+		var zoomOffset ={x:makePositive(zoomOriginSize.w*(1-rate)/2),y:makePositive(zoomOriginSize.h*(1-rate)/2)};//相对原点的偏移
+		var center ={w:makePositive(wrapSize.w-zoomSize.w)/2,y:makePositive(wrapSize.h-zoomSize.h)/2};//居中位置
+		var translate = {x:(center.x-zoomOffset.x)/rate,y:(center.y-zoomOffset.y)/rate};//缩放之后的偏移
+		if(value>100){
+			$zoomTarget.addClass("contentscale");
+		}else{
+			$zoomTarget.removeClass("contentscale");
+		}
+
+		if(value>fullzoomvalue){
+			$zoomWrap.css("overflow","auto");
+		}else{
+			$zoomWrap.css("overflow","hidden").scrollTop(0).scrollLeft(0);
+		}
+		$zoomTarget.css("transform","scale("+value/100+","+value/100+")"+"translate("+Math.floor(translate.x)+"px,"+Math.floor(translate.y)+"px)");
 	}
 }());
